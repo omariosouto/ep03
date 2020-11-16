d86060f5fd1b26a21193c5702213c1ddc7ea3011
Mario Souto <soutomarios@gmail.com>
2020-11-15 23:37:56 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GitHubCorner from './components/GitHubCorner';
+
+ReactDOM.render(<GitHubCorner />, document.querySelector('#app'))
